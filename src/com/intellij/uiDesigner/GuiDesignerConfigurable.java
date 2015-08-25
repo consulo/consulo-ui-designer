60d18be4ff5acf366fcbd909c2d11c60b7315a51
VISTALL <vistall.valeriy@gmail.com>
2015-08-25 19:28:09 +0300

    preparation

--- src/com/intellij/uiDesigner/GuiDesignerConfigurable.java
+++ src/com/intellij/uiDesigner/GuiDesignerConfigurable.java
@@ -15,8 +15,17 @@
  */
 package com.intellij.uiDesigner;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.codeInsight.CodeInsightUtil;
-import com.intellij.ui.ListCellRendererWrapper;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.diagnostic.Logger;
@@ -33,211 +42,268 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.PsiShortNamesCache;
+import com.intellij.ui.ListCellRendererWrapper;
 import com.intellij.uiDesigner.compiler.AsmCodeGenerator;
 import com.intellij.uiDesigner.make.FormSourceCodeGenerator;
 import com.intellij.uiDesigner.radComponents.LayoutManagerRegistry;
 import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
 
 /**
  * @author Anton Katilin
  * @author Vladimir Kondratyev
  */
-public final class GuiDesignerConfigurable implements SearchableConfigurable, Configurable.NoScroll {
-  private static final Logger LOG = Logger.getInstance("#com.intellij.uiDesigner.GuiDesignerConfigurable");
-  private final Project myProject;
-  private MyGeneralUI myGeneralUI;
+public final class GuiDesignerConfigurable implements SearchableConfigurable, Configurable.NoScroll
+{
+	private static final Logger LOG = Logger.getInstance("#com.intellij.uiDesigner.GuiDesignerConfigurable");
+	private final Project myProject;
+	private MyGeneralUI myGeneralUI;
 
-  /**
-   * Invoked by reflection
-   */
-  public GuiDesignerConfigurable(final Project project) {
-    myProject = project;
-  }
+	/**
+	 * Invoked by reflection
+	 */
+	public GuiDesignerConfigurable(final Project project)
+	{
+		myProject = project;
+	}
 
-  public String getDisplayName() {
-    return UIDesignerBundle.message("title.gui.designer");
-  }
+	@Override
+	public String getDisplayName()
+	{
+		return UIDesignerBundle.message("title.gui.designer");
+	}
 
-  @NotNull
-  public String getHelpTopic() {
-    return "project.propGUI";
-  }
+	@Override
+	@NotNull
+	public String getHelpTopic()
+	{
+		return "project.propGUI";
+	}
 
-  public JComponent createComponent() {
-    if (myGeneralUI == null) {
-      myGeneralUI = new MyGeneralUI();
-    }
+	@Override
+	public JComponent createComponent()
+	{
+		if(myGeneralUI == null)
+		{
+			myGeneralUI = new MyGeneralUI();
+		}
 
-    return myGeneralUI.myPanel;
-  }
+		return myGeneralUI.myPanel;
+	}
 
-  public boolean isModified() {
-    final GuiDesignerConfiguration configuration = GuiDesignerConfiguration.getInstance(myProject);
+	@Override
+	public boolean isModified()
+	{
+		final GuiDesignerConfiguration configuration = GuiDesignerConfiguration.getInstance(myProject);
 
-    if (myGeneralUI == null) {
-      return false;
-    }
+		if(myGeneralUI == null)
+		{
+			return false;
+		}
 
-    if (myGeneralUI.myChkCopyFormsRuntime.isSelected() != configuration.COPY_FORMS_RUNTIME_TO_OUTPUT) {
-      return true;
-    }
+		if(myGeneralUI.myChkCopyFormsRuntime.isSelected() != configuration.COPY_FORMS_RUNTIME_TO_OUTPUT)
+		{
+			return true;
+		}
 
-    if (myGeneralUI.myChkCopyForms.isSelected() != configuration.COPY_FORMS_TO_OUTPUT) {
-      return true;
-    }
+		if(myGeneralUI.myChkCopyForms.isSelected() != configuration.COPY_FORMS_TO_OUTPUT)
+		{
+			return true;
+		}
 
-    if (!Comparing.equal(configuration.DEFAULT_LAYOUT_MANAGER, myGeneralUI.myLayoutManagerCombo.getSelectedItem())) {
-      return true;
-    }
+		if(!Comparing.equal(configuration.DEFAULT_LAYOUT_MANAGER, myGeneralUI.myLayoutManagerCombo.getSelectedItem()))
+		{
+			return true;
+		}
 
-    if (!Comparing.equal(configuration.DEFAULT_FIELD_ACCESSIBILITY, myGeneralUI.myDefaultFieldAccessibilityCombo.getSelectedItem())) {
-      return true;
-    }
+		if(!Comparing.equal(configuration.DEFAULT_FIELD_ACCESSIBILITY, myGeneralUI.myDefaultFieldAccessibilityCombo
+				.getSelectedItem()))
+		{
+			return true;
+		}
 
-    if (configuration.INSTRUMENT_CLASSES != myGeneralUI.myRbInstrumentClasses.isSelected()) {
-      return true;
-    }
+		if(configuration.INSTRUMENT_CLASSES != myGeneralUI.myRbInstrumentClasses.isSelected())
+		{
+			return true;
+		}
 
-    if (configuration.RESIZE_HEADERS != myGeneralUI.myResizeHeaders.isSelected()) {
-      return true;
-    }
-    
-    return false;
-  }
+		if(configuration.RESIZE_HEADERS != myGeneralUI.myResizeHeaders.isSelected())
+		{
+			return true;
+		}
 
-  public void apply() {
-    final GuiDesignerConfiguration configuration = GuiDesignerConfiguration.getInstance(myProject);
-    configuration.COPY_FORMS_RUNTIME_TO_OUTPUT = myGeneralUI.myChkCopyFormsRuntime.isSelected();
-    configuration.COPY_FORMS_TO_OUTPUT = myGeneralUI.myChkCopyForms.isSelected();
-    configuration.DEFAULT_LAYOUT_MANAGER = (String)myGeneralUI.myLayoutManagerCombo.getSelectedItem();
-    configuration.INSTRUMENT_CLASSES = myGeneralUI.myRbInstrumentClasses.isSelected();
-    configuration.DEFAULT_FIELD_ACCESSIBILITY = (String)myGeneralUI .myDefaultFieldAccessibilityCombo.getSelectedItem();
-    configuration.RESIZE_HEADERS = myGeneralUI.myResizeHeaders.isSelected();
+		return false;
+	}
 
-    if (configuration.INSTRUMENT_CLASSES && !myProject.isDefault()) {
-      final DispatchThreadProgressWindow progressWindow = new DispatchThreadProgressWindow(false, myProject);
-      progressWindow.setRunnable(new MyApplyRunnable(progressWindow));
-      progressWindow.setTitle(UIDesignerBundle.message("title.converting.project"));
-      progressWindow.start();
-    }
-  }
+	@Override
+	public void apply()
+	{
+		final GuiDesignerConfiguration configuration = GuiDesignerConfiguration.getInstance(myProject);
+		configuration.COPY_FORMS_RUNTIME_TO_OUTPUT = myGeneralUI.myChkCopyFormsRuntime.isSelected();
+		configuration.COPY_FORMS_TO_OUTPUT = myGeneralUI.myChkCopyForms.isSelected();
+		configuration.DEFAULT_LAYOUT_MANAGER = (String) myGeneralUI.myLayoutManagerCombo.getSelectedItem();
+		configuration.INSTRUMENT_CLASSES = myGeneralUI.myRbInstrumentClasses.isSelected();
+		configuration.DEFAULT_FIELD_ACCESSIBILITY = (String) myGeneralUI.myDefaultFieldAccessibilityCombo
+				.getSelectedItem();
+		configuration.RESIZE_HEADERS = myGeneralUI.myResizeHeaders.isSelected();
 
-  public void reset() {
-    final GuiDesignerConfiguration configuration = GuiDesignerConfiguration.getInstance(myProject);
+		if(configuration.INSTRUMENT_CLASSES && !myProject.isDefault())
+		{
+			final DispatchThreadProgressWindow progressWindow = new DispatchThreadProgressWindow(false, myProject);
+			progressWindow.setRunnable(new MyApplyRunnable(progressWindow));
+			progressWindow.setTitle(UIDesignerBundle.message("title.converting.project"));
+			progressWindow.start();
+		}
+	}
+
+	@Override
+	public void reset()
+	{
+		final GuiDesignerConfiguration configuration = GuiDesignerConfiguration.getInstance(myProject);
 
     /*general*/
-    if (configuration.INSTRUMENT_CLASSES) {
-      myGeneralUI.myRbInstrumentClasses.setSelected(true);
-    }
-    else {
-      myGeneralUI.myRbInstrumentSources.setSelected(true);
-    }
-    myGeneralUI.myChkCopyFormsRuntime.setSelected(configuration.COPY_FORMS_RUNTIME_TO_OUTPUT);
-    myGeneralUI.myChkCopyForms.setSelected(configuration.COPY_FORMS_TO_OUTPUT);
+		if(configuration.INSTRUMENT_CLASSES)
+		{
+			myGeneralUI.myRbInstrumentClasses.setSelected(true);
+		}
+		else
+		{
+			myGeneralUI.myRbInstrumentSources.setSelected(true);
+		}
+		myGeneralUI.myChkCopyFormsRuntime.setSelected(configuration.COPY_FORMS_RUNTIME_TO_OUTPUT);
+		myGeneralUI.myChkCopyForms.setSelected(configuration.COPY_FORMS_TO_OUTPUT);
 
-    myGeneralUI.myLayoutManagerCombo.setModel(new DefaultComboBoxModel(LayoutManagerRegistry.getNonDeprecatedLayoutManagerNames()));
-    myGeneralUI.myLayoutManagerCombo.setRenderer(new ListCellRendererWrapper<String>() {
-      @Override
-      public void customize(JList list, String value, int index, boolean selected, boolean hasFocus) {
-        setText(LayoutManagerRegistry.getLayoutManagerDisplayName(value));
-      }
-    });
-    myGeneralUI.myLayoutManagerCombo.setSelectedItem(configuration.DEFAULT_LAYOUT_MANAGER);
+		myGeneralUI.myLayoutManagerCombo.setModel(new DefaultComboBoxModel(LayoutManagerRegistry
+				.getNonDeprecatedLayoutManagerNames()));
+		myGeneralUI.myLayoutManagerCombo.setRenderer(new ListCellRendererWrapper<String>()
+		{
+			@Override
+			public void customize(JList list, String value, int index, boolean selected, boolean hasFocus)
+			{
+				setText(LayoutManagerRegistry.getLayoutManagerDisplayName(value));
+			}
+		});
+		myGeneralUI.myLayoutManagerCombo.setSelectedItem(configuration.DEFAULT_LAYOUT_MANAGER);
 
-    myGeneralUI.myDefaultFieldAccessibilityCombo.setSelectedItem(configuration.DEFAULT_FIELD_ACCESSIBILITY);
-    
-    myGeneralUI.myResizeHeaders.setSelected(configuration.RESIZE_HEADERS);
-  }
+		myGeneralUI.myDefaultFieldAccessibilityCombo.setSelectedItem(configuration.DEFAULT_FIELD_ACCESSIBILITY);
 
-  public void disposeUIResources() {
-    myGeneralUI = null;
-  } /*UI for "General" tab*/
+		myGeneralUI.myResizeHeaders.setSelected(configuration.RESIZE_HEADERS);
+	}
 
-  private static final class MyGeneralUI {
-    public JPanel myPanel;
-    public JRadioButton myRbInstrumentClasses;
-    public JRadioButton myRbInstrumentSources;
-    public JCheckBox myChkCopyFormsRuntime;
-    private JComboBox myLayoutManagerCombo;
-    private JComboBox myDefaultFieldAccessibilityCombo;
-    private JCheckBox myResizeHeaders;
-    private JCheckBox myChkCopyForms;
-  }
+	@Override
+	public void disposeUIResources()
+	{
+		myGeneralUI = null;
+	}
 
-  private final class MyApplyRunnable implements Runnable {
-    private final DispatchThreadProgressWindow myProgressWindow;
+	private static final class MyGeneralUI
+	{
+		public JPanel myPanel;
+		public JRadioButton myRbInstrumentClasses;
+		public JRadioButton myRbInstrumentSources;
+		public JCheckBox myChkCopyFormsRuntime;
+		private JComboBox myLayoutManagerCombo;
+		private JComboBox myDefaultFieldAccessibilityCombo;
+		private JCheckBox myResizeHeaders;
+		private JCheckBox myChkCopyForms;
+		private JCheckBox myUseJBScalingCheckBox;
+	}
 
-    public MyApplyRunnable(final DispatchThreadProgressWindow progressWindow) {
-      myProgressWindow = progressWindow;
-    }
+	private final class MyApplyRunnable implements Runnable
+	{
+		private final DispatchThreadProgressWindow myProgressWindow;
 
-    /**
-     * Removes all generated sources
-     */
-    private void vanishGeneratedSources() {
-      final PsiShortNamesCache cache = PsiShortNamesCache.getInstance(myProject);
-      final PsiMethod[] methods = cache.getMethodsByName(AsmCodeGenerator.SETUP_METHOD_NAME, GlobalSearchScope.projectScope(myProject));
+		public MyApplyRunnable(final DispatchThreadProgressWindow progressWindow)
+		{
+			myProgressWindow = progressWindow;
+		}
 
-      CodeInsightUtil.preparePsiElementsForWrite(methods);
+		/**
+		 * Removes all generated sources
+		 */
+		private void vanishGeneratedSources()
+		{
+			final PsiShortNamesCache cache = PsiShortNamesCache.getInstance(myProject);
+			final PsiMethod[] methods = cache.getMethodsByName(AsmCodeGenerator.SETUP_METHOD_NAME,
+					GlobalSearchScope.projectScope(myProject));
 
-      for (int i = 0; i < methods.length; i++) {
-        final PsiMethod method = methods[i];
-        final PsiClass aClass = method.getContainingClass();
-        if (aClass != null) {
-          try {
-            final PsiFile psiFile = aClass.getContainingFile();
-            LOG.assertTrue(psiFile != null);
-            final VirtualFile vFile = psiFile.getVirtualFile();
-            LOG.assertTrue(vFile != null);
-            myProgressWindow.setText(UIDesignerBundle.message("progress.converting", vFile.getPresentableUrl()));
-            myProgressWindow.setFraction(((double)i) / ((double)methods.length));
-            if (vFile.isWritable()) {
-              FormSourceCodeGenerator.cleanup(aClass);
-            }
-          }
-          catch (IncorrectOperationException e) {
-            LOG.error(e);
-          }
-        }
-      }
-    }
+			CodeInsightUtil.preparePsiElementsForWrite(methods);
 
-    /**
-     * Launches vanish/generate sources processes
-     */
-    private void applyImpl() {
-      CommandProcessor.getInstance().executeCommand(myProject, new Runnable() {
-        public void run() {
-          ApplicationManager.getApplication().runWriteAction(new Runnable() {
-            public void run() {
-              PsiDocumentManager.getInstance(myProject).commitAllDocuments();
-              vanishGeneratedSources();
-            }
-          });
-        }
-      }, "", null);
-    }
+			for(int i = 0; i < methods.length; i++)
+			{
+				final PsiMethod method = methods[i];
+				final PsiClass aClass = method.getContainingClass();
+				if(aClass != null)
+				{
+					try
+					{
+						final PsiFile psiFile = aClass.getContainingFile();
+						LOG.assertTrue(psiFile != null);
+						final VirtualFile vFile = psiFile.getVirtualFile();
+						LOG.assertTrue(vFile != null);
+						myProgressWindow.setText(UIDesignerBundle.message("progress.converting",
+								vFile.getPresentableUrl()));
+						myProgressWindow.setFraction(((double) i) / ((double) methods.length));
+						if(vFile.isWritable())
+						{
+							FormSourceCodeGenerator.cleanup(aClass);
+						}
+					}
+					catch(IncorrectOperationException e)
+					{
+						LOG.error(e);
+					}
+				}
+			}
+		}
 
-    public void run() {
-      ProgressManager.getInstance().runProcess(new Runnable() {
-        public void run() {
-          applyImpl();
-        }
-      }, myProgressWindow);
-    }
-  }
+		/**
+		 * Launches vanish/generate sources processes
+		 */
+		private void applyImpl()
+		{
+			CommandProcessor.getInstance().executeCommand(myProject, new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					ApplicationManager.getApplication().runWriteAction(new Runnable()
+					{
+						@Override
+						public void run()
+						{
+							PsiDocumentManager.getInstance(myProject).commitAllDocuments();
+							vanishGeneratedSources();
+						}
+					});
+				}
+			}, "", null);
+		}
 
-  @NotNull
-  public String getId() {
-    return getHelpTopic();
-  }
+		@Override
+		public void run()
+		{
+			ProgressManager.getInstance().runProcess(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					applyImpl();
+				}
+			}, myProgressWindow);
+		}
+	}
 
-  @Nullable
-  public Runnable enableSearch(String option) {
-    return null;
-  }
+	@Override
+	@NotNull
+	public String getId()
+	{
+		return getHelpTopic();
+	}
+
+	@Override
+	@Nullable
+	public Runnable enableSearch(String option)
+	{
+		return null;
+	}
 }
